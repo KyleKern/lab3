89d945af60cf61d11790e268e4bb7f50fdc53bd3
Maria Loza <mloza@csumb.edu>
2017-09-14 22:41:46 +0000

    first init. setup ready.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lab 3</title>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
