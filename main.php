880fc9b0b47b340d95f11f94f7e6ebaca513d489
Dustin Grady <dustin.grady@gmail.com>
2017-09-20 00:08:32 -0700

    Add files via upload
    Added starter functions/ user images

---
+++ main.php
@@ -0,0 +1,85 @@
+<?php
+    $names = array(
+        "Bulbasaur",
+        "Ivysaur",
+        "Venusaur",
+        "Charmander",
+        "Charmeleon",
+        "Charizard",
+        "Squirtle",
+        "Wartortle",
+        "Blastoise",
+        "Caterpie",
+        "Metapod",
+        "Butterfree",
+        "Weedle",
+        "Kakuna",
+        "Beedrill",
+        "Pidgey",
+        "Pidgeotto",
+        "Pidgeot",
+        "Rattata",
+        "Raticate",
+        "Spearow",
+        "Fearow",
+        "Ekans",
+        "Arbok",
+        "Pikachu",
+        "Raichu",
+        "Sandshrew",
+        "Sandslash",
+        "Nidorina",
+        "Nidoqueen",
+        "Nidoran_male",
+        "Nidoran_female",
+        "Nidorino",
+        "Nidoking",
+        "Clefairy",
+        "Clefable",
+        "Vulpix",
+        "Ninetales");
+        
+    $players = array();
+    $scores = array();
+    $cards = array();
+        
+    function getPlayers($num){
+
+    }
+    
+    function getHand(){
+
+    }
+    
+    function displayWinner()
+    {
+
+    }
+?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+     <style>
+    @import url(styles/style.css);
+      
+      </style>
+       <h1 style="blue; background:gray "> Silver Jack </h1>
+     <meta charset="utf-8">
+     <title>Lab 3: Silverjack</title>
+</head>
+
+<body>
+    <hr> 
+    <?php
+        getPlayers(4);
+        getHand();
+        displayWinner();
+    ?>  
+    <hr/>
+</body>
+    <footer>
+
+    <footer/>
+</html>
