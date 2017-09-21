828409d5e2db1fef16a290967eb49dbbb5cec71a
KyleKern <kyle.z.kern@gmail.com>
2017-09-21 14:24:38 -0700

    Update index.php
    Fixed display winner to show winner closes to 42, also  to display message if nobody wins.

--- index.php
+++ index.php
@@ -117,28 +117,36 @@
     
  function displayWinner()
     {
-        global $names,$players,$score, $winnings;
+        global $names,$players,$score, $winnings,$topscore;
         $max=0;
-        
+        $winners=0;
         for($i = 0; $i < count($players); $i++)
         {
-            
             if($score[$i]>$max)
             {
                $max=$score[$i];
-               
             }
         }
-         for($i = 0; $i < count($players); $i++)
-        {
-            if($score[$i]<=42)
-            {
-                 
-               echo "<h1>" . $names[$players[$i]]." Wins ".$winnings=+$score[0]+$score[1]+$score[2]+$score[3]." points!" . "<hr/>";
-               echo "<br/>";
+        for($s=0;$s< count($players); $s++){
+                if($score[$s]<=42){
+                if($score[$s]>$topscore){
+                    $topscore=$score[$s];
+                }
             }
         }
-    }
+       
+        for($i=0; $i<count($players); $i++){
+            
+            if($score[$i]==$topscore){
+                echo "<h1>" . $names[$players[$i]]." Wins ".$winnings=+$score[0]+$score[1]+$score[2]+$score[3]." points!" . "<hr/>";
+                echo "<br/>";
+                $winners++;
+                }
+           }
+           if($winners==0){
+                echo "<h1> Nobody wins! <hr/>";
+            }
+        }
 ?>
 <!DOCTYPE html>
 
